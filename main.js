c809f93654f7706c0ca2d9f3b069c86fffbdff32
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-04 20:52:09 +0000

    fuck

--- main.js
+++ main.js
@@ -40,3 +40,18 @@ function endGame() {
     awayPoints -= awayPoints;
     awayScore.innerHTML = awayPoints;
 }
+
+
+
+
+
+function fizzBuzz(n) {
+    for(let i = 1; i <= n; i++){
+        if (i % 3 === 0 && i % 5 === 0) console.log(Fizzbuzz);
+        else if (i % 3 === 0 && i % 5 !== 0) console.log(Fizz);
+        else if (i % 5 === 0 && i % 3 !== 0) console.log(Buzz);
+        else if (i % 3 !== 0 && i % 5 !== 0) console.log(i);
+}
+}
+
+fizzBuzz(5)
