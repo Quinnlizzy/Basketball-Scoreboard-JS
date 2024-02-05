52203a471677f85c9e0a64e897a591f95f91a69a
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-05 16:13:47 +0000

    tidy

--- parseDocument.js
+++ parseDocument.js
@@ -5,7 +5,3 @@ function parseDocument(document)
     //split the doc into an array of lines
     let lines = documentWithoutLineOne.split(/\r\n|\r|\n/);
     let numLines = lines.length;
-    
-    //number of characters - just docu .length?
-    
-    let numCharacters = documentWithoutLineOne.length;
