cbb8e148e12826352067c1074b4f06de2c14802c
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-05 16:09:03 +0000

    tidy

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,6 +3,3 @@ function fizzBuzz(n) {
         if (i % 3 === 0 && i % 5 === 0) console.log('Fizzbuzz');
         else if (i % 3 === 0 && i % 5 !== 0) console.log('Fizz');
         else if (i % 5 === 0 && i % 3 !== 0) console.log('Buzz');
-        else if (i % 3 !== 0 && i % 5 !== 0) console.log(i);
-    }
-}
